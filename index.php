358189139a564796366cc9deedd578c0480045f0
AntonioSabella <asabella6@gmail.com>
2022-05-25 19:01:08 +0200

    Add method to set a new vote

--- index.php
+++ index.php
@@ -24,20 +24,42 @@ class Movie {
         $this->vote = $vote;
         $this->director = $director;
     }
+    
+    public function getVote()
+    {
+        return $this->vote;
+    }
+    public function setVote($vote)
+    {
+        $this->vote = $vote;
+    }
+
 
 }
 
+
+$matrix = new Movie('Matrix', 1999,'https://image.tmdb.org/t/p/w342/tiXqnZSlLpgsY38N3k4eaHiPbWu.jpg','English', 6.5, 'Lana Wachowski');
+$dune = new Movie('Dune: Part One', 2021,'https://image.tmdb.org/t/p/w342/a4DiBbHqXg91YQ8SLGL8otVqeOa.jpg', 'English', 7.8, 'Denis Villeneuve');
+$blade_runner = new Movie('Blade Runner 2049', 2017,'https://image.tmdb.org/t/p/w342/sPMX3MNTB0DqC6iCveIGKZN5AyN.jpg', 'English', 8.2, 'Denis Villeneuve',);
+
+$matrix-> setVote(4.5);
+$dune-> setVote(9.2);
+$blade_runner-> setVote(5.6);
+
 $movies = [
+    $matrix,
+    $dune,
+    $blade_runner,
     new Movie('Matrix', 1999,'https://image.tmdb.org/t/p/w342/tiXqnZSlLpgsY38N3k4eaHiPbWu.jpg','English', 6.5, 'Lana Wachowski'),
     new Movie('Dune: Part One', 2021,'https://image.tmdb.org/t/p/w342/a4DiBbHqXg91YQ8SLGL8otVqeOa.jpg', 'English', 7.8, 'Denis Villeneuve'),
-    new Movie('Blade Runner 2049', 2017,'https://image.tmdb.org/t/p/w342/2mPclQ5v9YK0eqCefvav3YsYPYI.jpg', 'English', 8.2, 'Denis Villeneuve',),
+    new Movie('Blade Runner 2049', 2017,'https://image.tmdb.org/t/p/w342/sPMX3MNTB0DqC6iCveIGKZN5AyN.jpg', 'English', 8.2, 'Denis Villeneuve',),
+    new Movie('Dune: Part One', 2021,'https://image.tmdb.org/t/p/w342/a4DiBbHqXg91YQ8SLGL8otVqeOa.jpg', 'English', 7.8, 'Denis Villeneuve'),
+    new Movie('Blade Runner 2049', 2017,'https://image.tmdb.org/t/p/w342/sPMX3MNTB0DqC6iCveIGKZN5AyN.jpg', 'English', 8.2, 'Denis Villeneuve',),
 ];
 
-
-
-//var_dump($matrix);
-//var_dump($dune);
-//var_dump($blade_runner);
+var_dump($matrix);
+var_dump($dune);
+var_dump($blade_runner);
 
 ?>
 
@@ -52,11 +74,11 @@ $movies = [
     <title>Movies</title>
     <style>
         main {
-            height: 100vh;
+            height: 100%;
         }
         img {
-            max-width:100%;
-            height: 600px;
+            max-width: 100%;
+            height: 500px;
         }
     </style>
 </head>
@@ -64,14 +86,14 @@ $movies = [
  <main>
 
     <div class="container-fluid bg-dark h-100">
-        <div class="row row-cols-3 align-items-center h-100">
+        <div class="row row-cols-4 g-3 align-items-center h-100">
             <?php foreach ($movies as $movie) : ?>
                 <div class="col bg-primary">
                     <div class="card text-center">
                         <img class="card-img-top" src="<?php echo $movie->poster_path; ?>" alt="Card image cap">
                         <div class="card-body">
                             <h4 class="card-text"><?php echo $movie->title; ?></h4>
-                            <small><?php echo $movie->vote; ?></small>
+                            <p><strong><?php echo $movie->vote; ?></strong></p>
                             <small><?php echo $movie->language; ?></small>
                             
                         </div>
